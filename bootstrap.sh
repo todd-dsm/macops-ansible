403d1e9a7b80db798d20ec689c1590d5fae3571c
Development <devr@MacBookPro.lan>
2025-09-20 11:26:28 -0700

    pulling vars from the env and using stat for validations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+# shellcheck source=/dev/null
 # Simple wrapper to run Ansible
-# Usage: ./run-ansible.sh [TEST] [additional ansible-playbook args]
-
+# Usage: ./bootstrap.sh [TEST] [additional ansible-playbook args]
 set -e
 
 # Get environment mode (TEST or LIVE)
 ENV_MODE="${1:-LIVE}"
-
 # Shift to get remaining arguments for ansible-playbook
 shift 2>/dev/null || true
+
+# Load variables into the environment quietly
+source ./my-vars.env >/dev/null 2>&1
 
 echo "Running Ansible with environment: $ENV_MODE"
 
--- lib/system-vars.sh
+++ lib/system-vars.sh
@@ -18,51 +18,51 @@ set -x
 # This stuff rarely-ever changes
 # -----------------------------------------------------------------------------
 # macOS Build
-myPath=''
-myMans=''
-configDir="${HOME}/.config"
-adminDir="${configDir}/admin"
-shellConfig="${configDir}/shell"
-adminLogs="${adminDir}/logs"
-backupDir="${adminDir}/backup"
-termDir="${configDir}/term"
-myShellDir="${configDir}/shell"
-sourceDir='sources'
-nvimDir="$configDir/nvim"
-sysShells='/etc/shells'
-gnuSed='/usr/local/opt/gnu-sed/libexec/gnubin/sed'
-gnuDate='/usr/local/opt/coreutils/libexec/gnubin/date'
-hostRemote='github.com'
-rawGHContent='https://raw.githubusercontent.com'
-myAnsibleDir="$HOME/.ansible"
-myAnsibleCFG="$myAnsibleDir/ansible.cfg"
-myAnsibleHosts="$myAnsibleDir/hosts"
-termStuff="$myDownloads"
-solarizedGitRepo='git@github.com:altercation/solarized.git'
+export myPath=''
+export myMans=''
+export configDir="${HOME}/.config"
+export adminDir="${configDir}/admin"
+export shellConfig="${configDir}/shell"
+export adminLogs="${adminDir}/logs"
+export backup_dir="${adminDir}/backup"
+export termDir="${configDir}/term"
+export myShellDir="${configDir}/shell"
+export sourceDir='sources'
+export nvimDir="$configDir/nvim"
+export sysShells='/etc/shells'
+export gnuSed='/usr/local/opt/gnu-sed/libexec/gnubin/sed'
+export gnuDate='/usr/local/opt/coreutils/libexec/gnubin/date'
+export host_remote='github.com'
+export rawGHContent='https://raw.githubusercontent.com'
+export myAnsibleDir="$HOME/.ansible"
+export myAnsibleCFG="$myAnsibleDir/ansible.cfg"
+export myAnsibleHosts="$myAnsibleDir/hosts"
+export termStuff="$myDownloads"
+export solarizedGitRepo='git@github.com:altercation/solarized.git'
 #------------------------------------------------------------------------------
-myShellProfile="$HOME/.zprofile"
-myShellrc="$HOME/.zshrc"
-myZSHExt="${myShellDir}/environment.zsh"
-myZSHAlias="${myShellDir}/aliases.zsh"
-myZSHFuncts="${myShellDir}/functions.zsh"
-myShellEnv="$HOME/.oh-my-zsh/custom"
+export myShellProfile="$HOME/.zprofile"
+export myShellrc="$HOME/.zshrc"
+export myZSHExt="${myShellDir}/environment.zsh"
+export myZSHAlias="${myShellDir}/aliases.zsh"
+export myZSHFuncts="${myShellDir}/functions.zsh"
+export myShellEnv="$HOME/.oh-my-zsh/custom"
 #------------------------------------------------------------------------------
-myGitConfig="$HOME/.gitconfig"
-myGitIgnore="$HOME/.gitignore"
-sysPaths='/etc/paths'
-sysManPaths='/etc/manpaths'
-# Configure Vim
-knownHosts="$HOME/.ssh/known_hosts"
-vimSimpleTag='" vimSimple configuration'
-vimSimpleLocal="$myCode/vimsimple"
-vimSimpleGitRepo='https://github.com/todd-dsm/vimSimple.git'
-pymodConfig="$vimSimpleLocal/vim/bundle/python-mode/plugin/pymode.vim"
-jsonIndent="$vimSimpleLocal/vim/bundle/vim-json/indent/json.vim"
-jsonIndREGEX='" =================$'
-jsonAppendStr='autocmd filetype json set et ts=2 sw=2 sts=2'
-# Configure macOS
-dirScreenshot="$myPics/screens"
-linkScreens="$myDesktop/screens"
+export myGitConfig="$HOME/.gitconfig"
+export myGitIgnore="$HOME/.gitignore"
+export sysPaths='/etc/paths'
+export sysManPaths='/etc/manpaths'
+export # Configure Vim
+export knownHosts="$HOME/.ssh/known_hosts"
+export vimSimpleTag='" vimSimple configuration'
+export vimSimpleLocal="$myCode/vimsimple"
+export vimSimpleGitRepo='https://github.com/todd-dsm/vimSimple.git'
+export pymodConfig="$vimSimpleLocal/vim/bundle/python-mode/plugin/pymode.vim"
+export jsonIndent="$vimSimpleLocal/vim/bundle/vim-json/indent/json.vim"
+export jsonIndREGEX='" =================$'
+export jsonAppendStr='autocmd filetype json set et ts=2 sw=2 sts=2'
+export # Configure macOS
+export dirScreenshot="$myPics/screens"
+export linkScreens="$myDesktop/screens"
 set +x
 
 # Test the last variable
@@ -75,4 +75,3 @@ else
     Initial configs look good. Let's do this!
     """
 fi
-
