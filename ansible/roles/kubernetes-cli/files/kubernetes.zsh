e40dd741c7836e96b10844d7e5be7fb685fd52eb
Todd E Thomas <todd.dsm@gmail.com>
2025-09-21 18:20:31 -0700

    added and tested kubernetes-cli install and config

---
+++ ansible/roles/kubernetes-cli/files/kubernetes.zsh
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------------- #
+#                                 KUBERNETES                                  #
+# --------------------------------------------------------------------------- #
+export MINIKUBE_IN_STYLE=false
+source <(minikube completion zsh)
+source "$HOME/.ktx"
+source "$HOME/.ktx-completion.sh"
